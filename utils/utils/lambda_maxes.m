f7b9216bbe05afa4ffa36913adade452cf4f89fe
Naoya Kumagai <75962438+naoya-kumagai@users.noreply.github.com>
2025-11-22 05:21:56 -0500

    remove unused utils functions

--- utils/utils/lambda_maxes.m
+++
@@ -1,6 +0,0 @@
-function arr = lambda_maxes(X)
-    arr = NaN(size(X,3),1);
-    for i = 1:size(X,3)
-        arr(i) = lambda_max(X(:,:,i));
-    end
-end
--- utils/utils/repblkdiag.m
+++
@@ -1,4 +0,0 @@
-function repM = repblkdiag(M, n)
-    C = repmat({M},n,1);
-    repM = blkdiag(C{:});
-end
